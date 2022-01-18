00dd01156e8589a99b339ab8325ab83c57a0976b
Benjamin <benng822@gmail.com>
2022-01-18 10:03:21 +0800

    setup config file and replace the log file location

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 import logging
+import os
 from logging.handlers import RotatingFileHandler
 
 from flask import Flask
 from flask.logging import default_handler
 
 app = Flask(__name__)
-app.config.from_object('config.DevelopmentConfig')
+
+# Configure the Flaks application
+config_type = os.getenv('CONFIG_TYPE', default='config.DevelopmentConfig')
+app.config.from_object(config_type)
 
 # Remove the default logger configure by flask
 app.logger.removeHandler(default_handler)
-app.secret_key = b'\xa2\xc2\x9e\x87P\xc8bv&0\xb3r`\xf4\x1d\x00\x95\x8f\x08E\x85\xe7\xc4\x97\xc0]\xa6\xa8\xb2\x1d %\xf6'
 
 # Logging Configuration
-file_handler = RotatingFileHandler(
-    'flask-stock-portfolio.log', maxBytes=16384, backupCount=20)
-file_formatter = logging.Formatter(
-    '%(asctime)s %(levelname)s: %(message)s [in %(filename)s:%(lineno)d]')
+file_handler = RotatingFileHandler('instance/flask-stock-portfolio.log', 
+                                    maxBytes=16384, 
+                                    backupCount=20)
+file_formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(filename)s:%(lineno)d]')
 file_handler.setFormatter(file_formatter)
 file_handler.setLevel(logging.INFO)
 app.logger.addHandler(file_handler)
